1040493c94a6663f80d7ec8ddde8b9bd544a576e
kirillkaz <kazazaev.kv@dns-shop.ru>
2026-04-14 00:42:09 +1000

    feat(cron_scheduler.py) Added scheduled task for clear redis cache

--- redis_practice/cron_scheduler.py
+++ redis_practice/cron_scheduler.py
@@ -2,9 +2,16 @@ from rq import cron
 
 from redis_practice.entities.typehints import QueuesEnum
 from redis_practice.tasks.gen_tasks import gen_orders_task
+from redis_practice.tasks.clear_cache import clear_cache_task
 
 cron.register(
     gen_orders_task,
     queue_name=QueuesEnum.GEN_QUEUE.value,
     interval=5,
+)
+
+cron.register(
+    clear_cache_task,
+    queue_name=QueuesEnum.CLEAR_QUEUE.value,
+    interval=60,
 )
